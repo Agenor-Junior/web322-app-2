f45c4c6dee6f6d0e7e6521cc5df87ec7f013e2ab
Agenor Junior <66803750+Agenor-Junior@users.noreply.github.com>
2024-12-24 16:32:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 WEB322 – Assignment 02
 I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
 No part of this assignment has been copied manually or electronically from any other source 
-(including 3rd party web sites) or distributed to other students.
+ or distributed to other students.
 
 Name: Agenor Dionizio da Silva Junior
 Student ID: 138121223
